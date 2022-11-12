f9863088e93af533ff2557754dc3cfba7a4e6dcd
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-12 01:08:51 +0100

    Added Google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "press-start": "'Press Start 2P', cursive",
+        "pt-sans": "'PT Sans', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
