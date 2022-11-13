65e797743647661e11e21e1fcb25b926bd003e47
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-13 21:24:03 +0100

    Added routes variables

---
+++ src/config/routes.ts
@@ -0,0 +1,6 @@
+export const INDEX_ROUTE = "/";
+
+export const HOME_PAGE_ROUTE = "/";
+export const PROJECTS_PAGE_ROUTE = "/projects";
+export const ABOUT_PAGE_ROUTE = "/about";
+export const CONTACT_PAGE_ROUTE = "/contact";
