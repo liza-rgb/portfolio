54210f271350dab33607bcc73406bf18f7cc362f
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-13 21:28:34 +0100

    Created Language Change Button component

---
+++ src/components/LangButton/LangButton.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
+const LangButton = () => {
+  const [lang, setLang] = useState(localStorage.lang || "EN");
+  const { i18n } = useTranslation();
+
+  const toggleLang = (translateTo: string) => {
+    switch (translateTo) {
+      case "UA":
+        localStorage.lang = "UA";
+        setLang("UA");
+        console.log(localStorage.lang, lang);
+        break;
+      case "EN":
+        localStorage.lang = "EN";
+        setLang("EN");
+        console.log(localStorage.lang, lang);
+        break;
+      default:
+        break;
+    }
+    window.location.reload();
+  };
+
+  const changeLanguage = (event: any) => {
+    console.log(localStorage.lang, lang);
+    const translateTo = event.target.innerHTML;
+    i18n.changeLanguage(translateTo.toLowerCase());
+    toggleLang(translateTo);
+  };
+
+  useEffect(() => {
+    i18n.changeLanguage(localStorage.lang.toLowerCase());
+  });
+
+  return (
+    <button type="button" onClick={changeLanguage}>
+      {lang === "EN" ? "UA" : "EN"}
+    </button>
+  );
+};
+
+export default LangButton;
