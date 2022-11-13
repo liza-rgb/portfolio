4b8303a69d2cb94876be6f47d3889f81824cd81b
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-14 00:10:57 +0100

    Created Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,7 +11,17 @@ const Home: React.FC = () => {
 
   return (
     <div>
-      <div className="absolute top-[40%] text-center text-grey-dark dark:text-grey-light z-10">
+      <div className="h-screen relative overflow-hidden">
+        <div className="bg-grey-dark bg-opacity-80 w-[600px] h-[600px] rounded-full absolute -bottom-[300px] -left-[300px] animate-appear"></div>
+        <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[300px] h-[300px] rounded-full absolute -bottom-[150px] left-[200px] animate-appear"></div>
+        <div className="bg-grey-dark bg-opacity-80 w-[200px] h-[200px] rounded-full absolute -bottom-[100px] left-[400px] animate-appear"></div>
+
+        <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[600px] h-[600px] rounded-full absolute -bottom-[300px] -right-[300px] animate-appear"></div>
+        <div className="bg-grey-dark bg-opacity-80 w-[300px] h-[300px] rounded-full absolute -bottom-[150px] right-[200px] animate-appear"></div>
+        <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[200px] h-[200px] rounded-full absolute -bottom-[100px] right-[400px] animate-appear"></div>
+      </div>
+
+      <div className="absolute top-[40%] text-center text-grey-dark dark:text-grey-light">
         <div className="w-screen font-press-start text-xl animate-appear">
           <span>{t("home.hello").toUpperCase()}</span>
           <span className="max-lg: block">{t("home.name").toUpperCase()}</span>
@@ -24,16 +34,6 @@ const Home: React.FC = () => {
           />
           <ArrowLink title={t("home.about-link")} link={ABOUT_PAGE_ROUTE} />
         </div>
-      </div>
-
-      <div className="h-screen relative overflow-hidden">
-        <div className="bg-grey-dark bg-opacity-80 w-[600px] h-[600px] rounded-full absolute -bottom-[300px] -left-[300px] animate-appear"></div>
-        <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[300px] h-[300px] rounded-full absolute -bottom-[150px] left-[200px] animate-appear"></div>
-        <div className="bg-grey-dark bg-opacity-80 w-[200px] h-[200px] rounded-full absolute -bottom-[100px] left-[400px] animate-appear"></div>
-
-        <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[600px] h-[600px] rounded-full absolute -bottom-[300px] -right-[300px] animate-appear"></div>
-        <div className="bg-grey-dark bg-opacity-80 w-[300px] h-[300px] rounded-full absolute -bottom-[150px] right-[200px] animate-appear"></div>
-        <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[200px] h-[200px] rounded-full absolute -bottom-[100px] right-[400px] animate-appear"></div>
       </div>
     </div>
   );
