e7820f6de7ec3a24aa167fa9efebf6fc2158e74e
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-15 00:55:54 +0100

    Updated Certificate Card component links

--- src/components/CertificateCard/CertificateCard.tsx
+++ src/components/CertificateCard/CertificateCard.tsx
@@ -32,7 +32,11 @@ const CertificateCard: React.FC<CertificateCardProps> = ({
         alt={title}
         className="h-[380px] rounded-[50px] mx-auto object-cover"
       />
-      <a href={link} title={t("about.view-certificate")}>
+      <a
+        href={link}
+        title={t("about.view-certificate")}
+        className="text-grey-dark hover:text-blue-bright dark:text-grey-light hover:dark:text-blue-light transition-colors duration-300"
+      >
         <h3 className="text-center text-md font-bold px-3 pt-5">{title}</h3>
       </a>
       <p className="text-center text-sm pt-1">{getDateString()}</p>
