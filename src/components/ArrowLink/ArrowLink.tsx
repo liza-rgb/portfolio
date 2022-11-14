150f1f4cf338691e53acbb51340ab47063300e09
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-14 01:40:40 +0100

    Updated Arrow Link component props

--- src/components/ArrowLink/ArrowLink.tsx
+++ src/components/ArrowLink/ArrowLink.tsx
@@ -4,13 +4,22 @@ import { ReactComponent as ArrowIcon } from "../../icons/arrow-right.svg";
 interface ArrowLinkProps {
   title: string;
   link: string;
+  linkTitle: string;
+  isTargetBlank?: boolean;
 }
 
-const ArrowLink: React.FC<ArrowLinkProps> = ({ title, link }) => {
+const ArrowLink: React.FC<ArrowLinkProps> = ({
+  title,
+  link,
+  linkTitle,
+  isTargetBlank = false,
+}) => {
   return (
     <a
       href={link}
-      title={title}
+      title={linkTitle}
+      target={isTargetBlank ? "_blank" : "_self"}
+      rel={isTargetBlank ? "noreferrer" : ""}
       className="flex space-x-1 text-blue-bright dark:text-blue-light"
     >
       <ArrowIcon className="fill-blue-bright dark:fill-blue-light animate-arrowPointer w-[24px] h-[24px]" />
