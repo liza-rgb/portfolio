64b8d873113ff26ab4583fc4616c821db9526690
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-17 00:15:11 +0100

    Added responsive behavior to Projects page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 import Accordion from "../../components/Accordion";
-import Footer from "../../components/Footer";
 import PageTitle from "../../components/PageTitle";
 
 import projectData from "./projectsData";
@@ -15,16 +14,10 @@ const Projects: React.FC = () => {
   }, []);
 
   return (
-    <div className="Projects pt-[110px] px-16">
-      <PageTitle
-        title={t("projects.title")}
-        className="text-grey-dark dark:text-grey-light animate-appear"
-      />
+    <div className="Projects">
+      <PageTitle title={t("projects.title")} />
       <div className="mt-10">
         <Accordion data={projectData} />
-      </div>
-      <div className="mt-6">
-        <Footer />
       </div>
     </div>
   );
