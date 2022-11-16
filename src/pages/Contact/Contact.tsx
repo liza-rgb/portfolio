667f891bc7114eeae139332eab6b24362c044b9e
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-17 00:15:29 +0100

    Added responsive behavior to Contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -12,7 +12,6 @@ import {
 
 import PageTitle from "../../components/PageTitle";
 import ContactLink from "./ContactLink";
-import Footer from "../../components/Footer";
 
 const Contact: React.FC = () => {
   const { t } = useTranslation();
@@ -23,50 +22,42 @@ const Contact: React.FC = () => {
 
   return (
     <div className="Contact">
-      <div className="pt-[110px] px-16">
-        <PageTitle
-          title={t("contact.title")}
-          className="text-grey-dark dark:text-grey-light animate-appear"
+      <PageTitle title={t("contact.title")} />
+      <div className="pt-10 space-y-5">
+        <ContactLink
+          title="Email"
+          content="lizz.stoliarchuk@gmail.com"
+          link={EMAIL_LINK}
+          linkTitle="Email Yelyzaveta"
         />
-        <div className="pt-10 space-y-5">
-          <ContactLink
-            title="Email"
-            content="lizz.stoliarchuk@gmail.com"
-            link={EMAIL_LINK}
-            linkTitle="Email Yelyzaveta"
-          />
-          <ContactLink
-            title="GitHub"
-            content="@liza_rgb"
-            link={GITHUB_LINK}
-            linkTitle="GitHub Profile"
-            isTargetBlank={true}
-          />
-          <ContactLink
-            title="LinkedIn"
-            content="Yelyzaveta Stoliarchuk"
-            link={LINKEDIN_LINK}
-            linkTitle="Linkedin Profile"
-            isTargetBlank={true}
-          />
-          <ContactLink
-            title="Facebook"
-            content="Liza Stoliarchuk"
-            link={FACEBOOK_LINK}
-            linkTitle="Facebook Profile"
-            isTargetBlank={true}
-          />
-          <ContactLink
-            title="Instagram"
-            content="@liza_stoliarchuk"
-            link={INSTAGRAM_LINK}
-            linkTitle="Instagram Profile"
-            isTargetBlank={true}
-          />
-        </div>
-      </div>
-      <div className="absolute bottom-0 w-screen">
-        <Footer />
+        <ContactLink
+          title="GitHub"
+          content="@liza_rgb"
+          link={GITHUB_LINK}
+          linkTitle="GitHub Profile"
+          isTargetBlank={true}
+        />
+        <ContactLink
+          title="LinkedIn"
+          content="Yelyzaveta Stoliarchuk"
+          link={LINKEDIN_LINK}
+          linkTitle="Linkedin Profile"
+          isTargetBlank={true}
+        />
+        <ContactLink
+          title="Facebook"
+          content="Liza Stoliarchuk"
+          link={FACEBOOK_LINK}
+          linkTitle="Facebook Profile"
+          isTargetBlank={true}
+        />
+        <ContactLink
+          title="Instagram"
+          content="@liza_stoliarchuk"
+          link={INSTAGRAM_LINK}
+          linkTitle="Instagram Profile"
+          isTargetBlank={true}
+        />
       </div>
     </div>
   );
