dbadaaf66fb6ff91c4f1154065f80b94a2fcd4e6
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-22 23:24:07 +0100

    Fixed mobile view of the Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -16,21 +16,23 @@ const Home: React.FC = () => {
   return (
     <div>
       <div className="h-screen relative overflow-hidden">
-        <div className="bg-grey-dark bg-opacity-80 w-[600px] h-[600px] rounded-full absolute -bottom-[300px] -left-[300px] animate-appear"></div>
-        <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[300px] h-[300px] rounded-full absolute -bottom-[150px] lg:left-[200px] left-[100px] animate-appear max-sm:hidden"></div>
+        <div className="bg-grey-dark bg-opacity-80 w-[600px] h-[600px] rounded-full absolute -bottom-[300px] -left-[300px] animate-appear max-sm:hidden"></div>
+        <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[300px] h-[300px] rounded-full absolute -bottom-[150px] -left-[150px] sm:left-[100px] lg:left-[200px] animate-appear"></div>
         <div className="bg-grey-dark bg-opacity-80 w-[200px] h-[200px] rounded-full absolute -bottom-[100px] lg:left-[400px] left-[280px] animate-appear max-md:hidden"></div>
 
-        <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[600px] h-[600px] rounded-full absolute -bottom-[300px] -right-[300px] animate-appear"></div>
-        <div className="bg-grey-dark bg-opacity-80 w-[300px] h-[300px] rounded-full absolute -bottom-[150px] lg:right-[200px] right-[100px] animate-appear max-sm:hidden"></div>
+        <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[600px] h-[600px] rounded-full absolute -bottom-[300px] -right-[300px] animate-appear max-sm:hidden"></div>
+        <div className="bg-grey-dark bg-opacity-80 w-[300px] h-[300px] rounded-full absolute -bottom-[150px] -right-[150px] sm:right-[100px] lg:right-[200px] animate-appear"></div>
         <div className="bg-blue-bright dark:bg-blue-light bg-opacity-80 w-[200px] h-[200px] rounded-full absolute -bottom-[100px] lg:right-[400px] right-[280px] animate-appear max-md:hidden"></div>
       </div>
 
       <div className="absolute top-[40%] text-center text-grey-dark dark:text-grey-light">
-        <h1 className="w-screen font-press-start text-xl animate-appear">
+        <h1 className="w-screen font-press-start text-[20px] xs:text-xl animate-appear">
           <span>{t("home.hello").toUpperCase()}</span>
           <span className="max-lg: block">{t("home.name").toUpperCase()}</span>
         </h1>
-        <h2 className="text-sm pt-3 animate-appear">{t("home.about")}</h2>
+        <h2 className="text-xs xs:text-sm pt-3 animate-appear">
+          {t("home.about")}
+        </h2>
         <div className="flex justify-center pt-5 space-x-8 text-sm animate-appear">
           <ArrowLink
             title={t("home.projects-link")}
